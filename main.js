da26af4ecb6dfe311020ecef994aa496b384f16d
Eren <vrsozluk@gmail.com>
2022-11-06 16:31:04 +0300

    part 1 has been done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let input = document.querySelector('.input');
 let button = document.querySelector('.button');
 let popup = document.querySelector('.popup');
 let intro = document.querySelector('.intro');
-
+let page2 = document.querySelector('.page2')
 
 
 const array = ["That's awesome! Anything else?", "That's craaaazy. What else you have got?", "That seems okay. Do you have any other experiences?", "That's impressive! What else!", "This class is amazing!, but is that all?", "I knew that Hazim Kulak is the best! Keep going!", "WoW! You guys are great! Anything else?"]
@@ -12,7 +12,7 @@ const array = ["That's awesome! Anything else?", "That's craaaazy. What else you
 
 function change(){
         if(input.value == "what is next?" ){
-                alert(1)
+                page2.classList.remove('hidden');
         }else if(input){
                 let randomNumber = Math.floor(Math.random() * (array.length));
                 intro.innerHTML = array[randomNumber]
